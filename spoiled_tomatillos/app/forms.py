c1b594c3b49e46ba62fe6518621a0fcf5686a139
Matt Morgan <mlylemorgan@gmail.com>
2018-02-15 04:07:45 -0500

    adding dataAlchemy structures and rough search

---
+++ spoiled_tomatillos/app/forms.py
@@ -0,0 +1,8 @@
+from wtforms import Form, SubmitField
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class SearchForm(Form):
+  search = StringField('search', [DataRequired()])
+  submit = SubmitField('Search',
+                       render_kw={'class': 'btn btn-success btn-block'})
--- spoiled_tomatillos/app/routes.py
+++ spoiled_tomatillos/app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template
+from flask import request
+
 from app import app
+from app.dbobjects import title_basic
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +20,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+#route for the search method
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == "POST":
+        results = title_basic.query.filter(title_basic.title.like('%' + request.form['search'] + '%')).all()
+        return render_template("results.html", allmovies=results)
+    return render_template('search.html')
