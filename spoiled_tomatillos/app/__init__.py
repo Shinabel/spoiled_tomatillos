76214b325e7ce3ce73de0294a035ba47c9608e9c
EC2 Default User <ec2-user@ip-172-31-19-104.us-west-2.compute.internal>
2018-02-27 04:04:13 +0000

    Added PyMySQL library to replace MySQLdb for AWS server

--- spoiled_tomatillos/app/__init__.py
+++ spoiled_tomatillos/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config.from_object(Config)
