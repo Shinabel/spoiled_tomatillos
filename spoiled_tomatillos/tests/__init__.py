4f3f212a50aa1cfc0086e19e83b2e2ecb90d4800
Jay Lok <lok.j@husky.neu.edu>
2018-03-15 13:56:53 -0400

    TEAM53-92 #comment missed an import

--- spoiled_tomatillos/tests/__init__.py
+++ spoiled_tomatillos/tests/__init__.py
@@ -2,4 +2,4 @@ import os
 os.environ["FLASK_APP"] = "cs4500.py"
 os.environ["APP_MAIL_USERNAME"] = "spoiledtomatillos53@gmail.com"
 os.environ["APP_MAIL_PASSWORD"] = "cs4500project"
-from app import app
+#from app import app
