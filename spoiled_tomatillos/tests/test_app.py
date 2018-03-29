8d17e6f949de1ff113d3c1caf12af759690b73db
Jay Lok <lok.j@husky.neu.edu>
2018-03-29 03:10:42 -0400

    TEAM53-102 update pytest fixtures and basic import testing

--- spoiled_tomatillos/tests/test_app.py
+++ spoiled_tomatillos/tests/test_app.py
@@ -1,18 +1,58 @@
 import unittest
 import os
-from app import app as test_app
 from config import test_config
 import pytest
+#import ipdb
 
+'''
 @pytest.fixture
 def app():
-    pass
+    return test_app
+'''
 
 @pytest.fixture
-def client(app):
-    return test_app.test_client()
+def client():
+    """ Fixture setting up testing environemtn
+    TODO: Add more environment info, sepatate testing config
+    """
+    from app import app
+    client = app.test_client()
+    yield client
 
-class MyTestClass(unittest.TestCase): 
+def test_app(client):
+    pass
+
+def test_db_object(client):
+    from app import db
+
+def test_login_manager(client):
+    from app import login_manager
+
+def test_create_app(client):
+    from app import create_app
+    create_app()
+
+def test_test_config(client):
+    from config import Config
+    from config import test_config
+
+def test_dbobjects(client):
+    from app import dbojects
+
+def test_email(client):
+    from app import email
+
+def test_pdb(client):
+    pytest.set_trace()
+    try:
+        if os.environ["JAY_TEST"] == "True":
+            pytest.set_trace()
+    except:
+        pass
+
+
+'''
+class spoiled_tomatillos_test_class(unittest.TestCase): 
     # initialization logic for the test suite declared in the test module
     # code that is executed before all tests in one test run
     @classmethod
@@ -41,8 +81,7 @@ class MyTestClass(unittest.TestCase):
     def test_equal_numbers(self):
         self.assertEqual(2, 2) 
         
-    
-
 # runs the unit tests in the module
 if __name__ == '__main__':
     unittest.main()
+'''
