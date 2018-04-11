a09def8cfce542d9198820952a4c78dc7e704bb6
Jay Lok <lok.j@husky.neu.edu>
2018-04-11 13:50:31 -0400

    TEAM53-140 Fixing tabs inside email

--- spoiled_tomatillos/app/email.py
+++ spoiled_tomatillos/app/email.py
@@ -1,9 +1,9 @@
 from flask_mail import Message
-
+import logging
 from app import app, mail
 
 def send_email(to, subject, template):
-	logging.info("Sending email to: {} \n Using template: ")
+    logging.info("Sending email to: {} \n Using template: ")
     msg = Message(
         subject,
         recipients=[to],
